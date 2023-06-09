a0e6091a0822a87c846aa87dba66a12948db7bf2
LySabrina <lysabrinak@gmail.com>
2023-06-09 16:46:32 -0700

    v0.1

---
+++ Releases/src/app/src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//Display the user's profile 
+//Photo, Reviews on Book, Top 3 fav books
+export default function Profile() {
+  return (
+    <div>Profile</div>
+  )
+}
